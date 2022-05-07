6271c22494a79c06a31846a64b5678e3dce30ac7
Haidee <99485862+HaideeCardy@users.noreply.github.com>
2022-05-07 10:36:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-/////////////////// Open Weather API II //////////////////////////
+// Search Bar //
+
 let search = (e) => {
   const keyAPI = `b0929492df7fe554988cfbc8cb3a2505`;
   const loc = document.getElementById('locInput').value;
   const stringPassIn = `http://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${keyAPI}`;
 
+  // Fetching the Data //
+  
   const getData = () => {
-    fetch(stringPassIn)
-      .then((res) => res.json())
+    fetch(stringPassIn).      //pulling in API//
+      .then((res) => res.json())      //convert to json//
       .then((data) => {
-        console.log(data);
+        console.log(data);     // data to be displayed //
         const description = data.weather[0].description.toUpperCase();
         const temperature = Math.trunc(data.main.temp) - 273;
         const feelsLike = Math.trunc(data.main.feels_like) - 273;
@@ -16,12 +19,12 @@ let search = (e) => {
         document.getElementById(
           'results'
         ).innerHTML = `<p>${description} <br>Temperature <b>${temperature} °C</b> <br>But Feels Like <b>${feelsLike} °C</b>  <br>Humidity <b>${humidity}% </b></p>`;
-      })
-      .catch((err) => console.log(err));
+      })    // Create and display the results in HTML //
+      .catch((err) => console.log(err));    // 1 catch to log Error //
   };
   getData();
   e.preventDefault();
 };
 
-document.getElementById('myBTN').addEventListener('click', search);
+document.getElementById('myBTN').addEventListener('click', search);   // event listener to link button //
 
